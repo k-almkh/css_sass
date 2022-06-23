580fa23ae561872ae79bf65e415aeaf7730a0c2f
k-almkh <almkheterkhaled@gmail.com>
2022-06-23 15:19:34 +0200

    dentist2

--- dentist_website/js/script.js
+++ dentist_website/js/script.js
@@ -4,6 +4,10 @@ let header = document.querySelector('.header');
 
 let topBtn = document.querySelector('#myTopBtn');
 
+let accordionBtn = document.querySelectorAll('.accordion_header');
+
+
+
 menu.onclick = () =>{
    menu.classList.toggle('fa-times');
    navbar.classList.toggle('active');
@@ -26,7 +30,20 @@ window.onscroll = () =>{
    }
 
 }
+
+// To Top
 topBtn.onclick = () =>{
    document.body.scrollTop = 0;
    document.documentElement.scrollTop = 0;
 }
+
+//  Animated Accordion 
+accordionBtn.forEach(function (elem) {
+   elem.addEventListener('click', function handleClick(event) {
+     // ✅ Remove class from each element
+      accordionBtn.forEach(elem => {
+          elem.classList.remove('accordion_active');
+       });
+      event.target.classList.add("accordion_active");
+    });
+});
